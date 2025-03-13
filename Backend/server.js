4a3c4e55ad29c4c68c285b3f9c42c35e83b7b170
cicloIT <desarrollo@cicloit.com>
2025-03-13 09:05:48 -0300

    change estado

--- Backend/server.js
+++ Backend/server.js
@@ -596,7 +596,7 @@ app.post("/clientes", async (req, res) => {
 
 /* Presupuesto */
 app.post("/presupuestos", verificarToken, async (req, res) => {
-  const { nombreCliente, descripcion, productos, servicios, accesorios, total,estado} = req.body;
+  const { nombreCliente, descripcion, productos, servicios, accesorios, total} = req.body;
   // Convertir total a número real para mayor seguridad
   const totalPresupuesto = parseFloat(total);
 
@@ -642,7 +642,7 @@ app.post("/presupuestos", verificarToken, async (req, res) => {
     productosText = productosText ? productosText.slice(0, -2) : "";
     serviciosText = serviciosText ? serviciosText.slice(0, -2) : "";
     accesoriosText = accesoriosText ? accesoriosText.slice(0, -2) : "";
-    estado = 'pendiente';
+    const estado = "pendiente"; 
     // Paso 4: Insertar el presupuesto en la tabla `presupuesto`
     const resultPresupuesto = await db.presupuesto.execute({
       sql: "INSERT INTO presupuesto (nombre_cliente, descripcion, productos, servicios, accesorios, total, estado) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
