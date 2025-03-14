5fd82242ef26efb732c9a5edb7e7e0c6b9afb859
cicloIT <desarrollo@cicloit.com>
2025-03-14 13:18:59 -0300

    cambiando dependencia sqlite

--- Backend/db.js
+++ Backend/db.js
@@ -1,18 +1,22 @@
 import dotenv from 'dotenv';
-import { createClient } from '@libsql/client/web';
+import { createClient } from '@libsql/client';
 
 dotenv.config();
 
 // Configura el cliente para productos, servicios y accesorios
 const client = createClient({
-  url: 'https://cicloit-samuelnar.turso.io',   
+  url: 'libsql://cicloit-samuelnar.turso.io',   
   authToken: process.env.DB_TOKEN,
+  tls: true,
+  syncUrl: 'https://cicloit-samuelnar.turso.io' // URL HTTP en lugar de WebSockets
 });
 
 // Configura el cliente para presupuestos
 const presupuesto = createClient({
-  url: 'https://presupuesto-samuelnar.turso.io',
+  url: 'libsql://presupuesto-samuelnar.turso.io',
   authToken: process.env.DB_TOKEN_PRESUPUESTO,
+  tls: true,
+  syncUrl: 'https://presupuesto-samuelnar.turso.io' // URL HTTP en lugar de WebSockets
 });
 
 export default { client, presupuesto };
