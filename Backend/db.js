7395550bf60d46e66e5520a9b00fecf0979cb132
cicloIT <desarrollo@cicloit.com>
2025-04-14 15:31:07 -0300

    Change repository

--- Backend/db.js
+++ Backend/db.js
@@ -5,13 +5,13 @@ dotenv.config();
 
 // Configura el cliente para productos, servicios y accesorios
 const client = createClient({
-  url: 'libsql://cicloit-samuelnar.turso.io',   
+  url: 'libsql://cicloit-cicloit.aws-us-east-1.turso.io',     
   authToken: process.env.DB_TOKEN,  // Token de autenticación para la base de datos de productos, servicios, etc.
 });
 
 // Configura el cliente para presupuestos
 const presupuesto = createClient({
-  url: 'libsql://presupuesto-samuelnar.turso.io',
+  url: 'libsql://presupuesto-cicloit.aws-us-east-1.turso.io',
   authToken: process.env.DB_TOKEN_PRESUPUESTO  // Token de autenticación para la base de datos de presupuestos
 });
 
